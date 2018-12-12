95f577da6f423854949df69dad8d8e9c8f662e4e
AFU <Password2191>
2018-12-12 13:06:04 +0100

    Adem er sejest

--- index.php
+++ index.php
@@ -37,7 +37,7 @@ require("db/db.php");
 <meta property="og:locale" content="da_DK">
 
 <!-- Sikrer man kan benytte CSS ved at tilkoble en CSS fil -->
-<link href="css/styles.css" rel="stylesheet" type="text/css">
+<link href="css/ademcss.css" rel="stylesheet" type="text/css">
     <link href="https://fonts.googleapis.com/css?family=Amatic+SC" rel="stylesheet">
     <link rel="stylesheet" href="https://use.typekit.net/una2owl.css">
 <!-- Sikrer den vises korrekt på mobil, tablet mv. ved at tage ift. skærmstørrelse -->

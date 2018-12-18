8773ce8c192fd0d0dbadb0f7be3ab75f1acfeab4
MikkelBB <mikkelbis@gmail.com>
2018-12-18 11:00:42 +0100

    styles

--- produkterindividuel.php
+++ produkterindividuel.php
@@ -1,3 +1,8 @@
+<?php
+require("db/db.php");
+
+?>
+
 <!doctype html>
 <!-- Fortæller det er html5 -->
 <!-- html starter og slutter hele dokumentet / lang=da: Fortæller siden er på dansk -->
@@ -27,7 +32,7 @@
 <meta property="og:image" content="http://www.domain.dk/images/jpg">
 <meta property="og:description" content="Beskrivelse">
 <meta property="og:locale" content="da_DK">
-
+<script defer src="https://use.fontawesome.com/releases/v5.0.8/js/all.js"></script>
 <!-- Sikrer man kan benytte CSS ved at tilkoble en CSS fil -->
 <link href="css/styles.css" rel="stylesheet" type="text/css">
     <link href="https://fonts.googleapis.com/css?family=Amatic+SC" rel="stylesheet">
@@ -39,5 +44,35 @@
 
 <!-- i <body> har man alt indhold på siden -->
 <body>
+
+<?php
+
+$readId = $_GET["id"];
+
+$produkterQuery = mysqli_query($db, "SELECT * FROM produkter WHERE pId = '$readId'");
+$produkter = mysqli_fetch_assoc($produkterQuery);
+
+/*
+echo "<div class='MoviePicture'><img src='images/mwm/".$movies["mBillede"]."'></div>";
+echo "<div class='Moviebox'><div class='MovieNavn'>".$movies["mName"].$movies["mGenre"].$movies["mAarstal"].$movies["mTid"]
+     .$movies["mRating"].$movies["mBeskrivelse"]."</div></div>"*/
+
+echo "<div class='produkterPicture'><img src='images".$produkter["pBillede"]."'></div>";
+
+echo "<div class='produkterflex'><div class='produkternavn'><h2>".$produkter["pNavn"]."</h2></div>".
+    "<div class='produkterpris'>".$produkter["pPris"]."</div>".
+    "<div class='produkterbeskrivelse'>".$produkter["pbeskrivelse"]."</div>".
+    "<div class='produkterkategori'><p>Kategori: </p>".$produkter["pKategori"]."</div></div>";
+
+
+echo "<a href='#'><div class='addkurv'><i class='fas fa-plus'></i><p>Tilføj til Kurv</p></div></a>";
+
+
+?>
+
+
+
+
+
 </body>
 </html>
